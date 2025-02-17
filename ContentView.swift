7fbda7826f833f70227f232f1dfe24f379a29060
yudo417 <hayashi.kazuki.hk@gmail.com>
2025-02-17 14:03:51 +0900

    mae

--- ContentView.swift
+++ ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import RealityKit
 import ARKit
+import UIKit
 
 struct ContentView: View {
     var body: some View {
@@ -43,18 +44,32 @@ struct ARViewContainer: UIViewRepresentable {
     class Coordinator:NSObject,ARSessionDelegate {
         var parent : ARViewContainer
 
+        private var IsPlacedObject = false
+
         init(_ parent: ARViewContainer) {
             self.parent = parent
         }
 
         nonisolated func session(_ session:ARSession,didAdd anchors:[ARAnchor]){
+
+
+
             Task { @MainActor [weak self] in
                             guard let self = self else { return }
+
+                guard !self.IsPlacedObject else { return }
+
+
                             self.handleDidAddAnchors(anchors)
                         }
         }
 
         func handleDidAddAnchors(_ anchors:[ARAnchor]) {
+            let _ = print("\(IsPlacedObject)")
+
+            guard !IsPlacedObject else { return }
+
+
             for anchor in anchors {
 
                 if let planeAnchor = anchor as? ARPlaneAnchor {
@@ -63,14 +78,30 @@ struct ARViewContainer: UIViewRepresentable {
 
 
                     // ARセッションの設定
-                    if let usdzModel = try? ModelEntity.loadModel(named: "1_9") {
+                    if let usdzModel = try? ModelEntity.loadModel(named: "questionBox2") {
                         usdzModel.transform = Transform(
                             scale: [0.04,0.04,0.04], rotation: simd_quatf(angle:  -.pi/2, axis: [1,0,0]), translation: [0,0,0]
                             )
                         anchorEntity.addChild(usdzModel)
                         print("あった")
+
+                        let animations = usdzModel.availableAnimations
+                                               if let firstAnim = animations.first {
+                                                   // 無限ループで再生したい場合
+                                                   usdzModel.playAnimation(
+                                                       firstAnim,
+                                                       transitionDuration: 0.2,
+                                                       startsPaused: false
+                                                   )
+
+                                                   print("アニメーションを再生開始")
+                                               } else {
+                                                   print("アニメーションが見つからなかった")
+                                               }
                     }
                     parent.arView.scene.addAnchor(anchorEntity)
+
+                    IsPlacedObject = true
 
                 }
             }
