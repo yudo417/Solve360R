3438a6236584d6f68b5ddd586bbce1726c280d96
yudo417 <hayashi.kazuki.hk@gmail.com>
2025-02-18 17:20:02 +0900

    can equation temp

--- ContentView.swift
+++ ContentView.swift
@@ -10,10 +10,16 @@ import RealityKit
 import ARKit
 import UIKit
 
+enum PutItemKind {
+    case NumberBox
+    case QuestionBox
+    case sign
+}
+
 struct ContentView: View {
     var body: some View {
         ARViewContainer()
-        .padding()
+            .padding()
     }
 }
 
@@ -26,8 +32,8 @@ struct ARViewContainer: UIViewRepresentable {
 
     func makeUIView(context: Context) -> ARView {
         let configuration = ARWorldTrackingConfiguration()
-                configuration.planeDetection = [.horizontal]
-                arView.session.run(configuration)
+        configuration.planeDetection = [.horizontal]
+        arView.session.run(configuration)
 
         arView.session.delegate = context.coordinator
         return arView
@@ -55,13 +61,13 @@ struct ARViewContainer: UIViewRepresentable {
 
 
             Task { @MainActor [weak self] in
-                            guard let self = self else { return }
+                guard let self = self else { return }
 
                 guard !self.IsPlacedObject else { return }
 
 
-                            self.handleDidAddAnchors(anchors)
-                        }
+                self.handleDidAddAnchors(anchors)
+            }
         }
 
         func handleDidAddAnchors(_ anchors:[ARAnchor]) {
@@ -75,38 +81,51 @@ struct ARViewContainer: UIViewRepresentable {
                 if let planeAnchor = anchor as? ARPlaneAnchor {
 
                     let planePosition = SIMD3<Float>(
-                                            planeAnchor.transform.columns.3.x,
-                                            planeAnchor.transform.columns.3.y,
-                                            planeAnchor.transform.columns.3.z
-                                        )
+                        planeAnchor.transform.columns.3.x,
+                        planeAnchor.transform.columns.3.y,
+                        planeAnchor.transform.columns.3.z
+                    )
                     let anchorEntity = AnchorEntity(world: planePosition)
 
 
                     // ARセッションの設定
-                    if let usdzModel = try? ModelEntity.loadModel(named: "4_7") {
-                        usdzModel.transform = Transform(
-                            scale: [0.04,0.04,0.04], rotation: simd_quatf(angle:  -.pi/2, axis: [1,0,0]) * simd_quatf(angle:  -.pi/2, axis: [0,0,1]), translation: [0,0,0]
-                            )
-                        anchorEntity.addChild(usdzModel)
-                        print("あった")
-
-                        parent.arView.scene.addAnchor(anchorEntity)
-                        
-                    }
-
-                    if let usdzModel = try? ModelEntity.loadModel(named: "8_3") {
-                        usdzModel.transform = Transform(
-                            scale: [0.04,0.04,0.04], rotation: simd_quatf(angle:  -.pi/2, axis: [1,0,0]) * simd_quatf(angle:  -.pi/2, axis: [0,0,1]), translation: [0,0,-0.2]
-                            )
-                        anchorEntity.addChild(usdzModel)
-                        print("あった")
-
-                        parent.arView.scene.addAnchor(anchorEntity)
-                        
-                    }
+                    PutItem(translation: [-0.4,0,0], itemname: "4_7", anchorentity: anchorEntity,putitemkind: .NumberBox,IsRotation: true)
+                    PutItem(translation: [-0.2,0,0], itemname: "sign", anchorentity: anchorEntity,putitemkind: .sign)
+                    PutItem(translation: [0,0,0], itemname: "questionBox", anchorentity: anchorEntity,putitemkind: .QuestionBox)
+                    PutItem(translation: [0.2,0,0], itemname: "sign", anchorentity: anchorEntity,putitemkind: .sign)
+                    PutItem(translation: [0.4,0,0], itemname: "8_3", anchorentity: anchorEntity,putitemkind: .NumberBox,IsRotation: true)
 
                     IsPlacedObject = true
 
+                }
+            }
+        }
+
+        func PutItem(translation:SIMD3<Float>,itemname:String,anchorentity:AnchorEntity,putitemkind:PutItemKind,IsRotation:Bool = false){// -.pi/2z軸基準に回転制御
+            switch putitemkind{
+            case .NumberBox:
+                if let usdzModel = try? ModelEntity.loadModel(named:itemname){
+                    usdzModel.transform = Transform(
+                        scale: [0.04,0.04,0.04], rotation: simd_quatf(angle:  -.pi/2, axis: [1,0,0]) * simd_quatf(angle: IsRotation ?  -.pi/2 : 0 , axis: [0,0,1]), translation:translation
+                    )
+                    anchorentity.addChild(usdzModel)
+                    parent.arView.scene.addAnchor(anchorentity)
+                }
+            case .sign:
+                if let usdzModel = try? ModelEntity.loadModel(named:itemname){
+                    usdzModel.transform = Transform(
+                        scale: [0.04,0.04,0.04], rotation: simd_quatf(angle:  .pi/2, axis: [1,0,0]) * simd_quatf(angle: IsRotation ?  -.pi/2 : .pi , axis: [0,1,0]), translation:translation
+                    )
+                    anchorentity.addChild(usdzModel)
+                    parent.arView.scene.addAnchor(anchorentity)
+                }
+            case .QuestionBox:
+                if let usdzModel = try? ModelEntity.loadModel(named:itemname){
+                    usdzModel.transform = Transform(
+                        scale: [0.04,0.04,0.04], rotation: simd_quatf(angle:  -.pi/2, axis: [1,0,0]) * simd_quatf(angle:  -.pi/2, axis: [0,0,1]), translation:translation
+                    )
+                    anchorentity.addChild(usdzModel)
+                    parent.arView.scene.addAnchor(anchorentity)
                 }
             }
         }
