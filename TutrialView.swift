cd5cd50021fa247fc2abcb931de0a32c9cf24fd7
yudo417 <hayashi.kazuki.hk@gmail.com>
2025-02-23 10:35:34 +0900

    finish temp

---
+++ TutrialView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  SpinSolve360
+//
+//  Created by 林　一貴 on 2025/02/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
